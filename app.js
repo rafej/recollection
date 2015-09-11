0adfaea1f1c00ae5655e8c1312a1803a4630650e
rafej <rafe.s@hotmail.com>
2015-09-11 22:10:30 +1000

    Started Angular Routes

--- app.js
+++ app.js
@@ -1,4 +1,43 @@
-angular.module('recollection')
-	.config(function($routeProvider) {
-		
-	});
+var recollection = angular.module('recollection',['ngRoute']);
+
+recollection.config(function($routeProvider) {
+		$routeProvider
+
+    //route for home page
+		.when('/', {
+			template:'views/home.html',
+			controller  : 'mainController'
+    })
+		.when('/login' , {
+    template: 'views/login.html',
+    controller : 'LoginCtrl'
+  		})
+     .when('/signup', {
+  	template: 'views/signup.html',
+    		controller: 'signupController'
+ 	 })
+  	.when('/progress', {
+  	template:'views/progress.html',
+  	controller:'progressController'
+  })
+  .when('/questions', {
+  	template:'views/questions.html',
+  	controller:'questionsController'
+  		});
+    });
+   recollection.controller('mainController',function($scope){
+    $scope.message = "does this work?" ;
+   });
+   recollection.controller('loginController', function($scope){
+     
+   });
+   recollection.controller('signupController', function($scope){
+     
+   });
+    recollection.controller('progressController', function($scope){
+     
+   });
+     recollection.controller('questionsController', function($scope){
+     
+   });
+      
